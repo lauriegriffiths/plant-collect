9b71b59fb925201ae294f7ec8707d662d9e26554
Laurie Griffiths <laurie@steampunk.digital>
2023-01-28 17:51:48 +0900

    alien tree

--- Assets/InputHandler.cs
+++ Assets/InputHandler.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
+    private GameObject tree;
     public HingeJoint2D hinge;
+    private Vector3 scaleChange;
     // Start is called before the first frame update
     void Start()
     {
         hinge = gameObject.GetComponent<HingeJoint2D>();
-
+        scaleChange = new Vector3(0.001f, 0.001f, 0.0f);
+        tree = GameObject.Find("Tree");
         
     }
 
@@ -30,7 +33,11 @@ public class InputHandler : MonoBehaviour
             hinge.motor = motor;
             hinge.useMotor = true;
         }
-        else{
+        else if(Input.GetKey("q")){
+            // Make the hinge motor rotate with 90 degrees per second and a strong force.
+            tree.transform.localScale += scaleChange;
+        }
+        else {
             var motor = hinge.motor;
             motor.motorSpeed = 0;
             hinge.motor = motor;
